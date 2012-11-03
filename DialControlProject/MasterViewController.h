538e8a2d7f321d5b0e3b9590e0b4637de9c92fed
Brad Eaton <brad@Brads-MacBook-Air.local>
2012-11-02 22:37:20 -0500

    Initial Commit

---
+++ DialControlProject/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  DialControlProject
+//
+//  Created by Brad Eaton on 11/2/12.
+//  Copyright (c) 2012 Brad Eaton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
